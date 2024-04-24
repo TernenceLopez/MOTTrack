cafb18d9d2d4ff4955c36aeef3d5485aa92baf75
TernenceLopez <ternencelopez@gmail.com>
2024-04-24 19:04:16 +0800

    refactor: 增加yolov8知识蒸馏相关常量

--- ReIDNet/distillation_utils/constant_param.py
+++ ReIDNet/distillation_utils/constant_param.py
@@ -1,5 +1,7 @@
 class opt:
     ratio = 0.25
-    kd_loss_selected = 'l2' # 软标签损失的损失函数
+    kd_loss_selected = 'l2'  # 软标签损失的损失函数
     temperature = 20
     isL = False
+    yolo_kd_switch = True  # yolov8知识蒸馏开关
+    teacher_model = "./teacher_model.pt"
